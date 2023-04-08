4f5dcfa6fb099d59274ba5683b2e7abafb771e26
João Pedro <joaoplay16@gmail.com>
2023-04-08 18:11:47 -0300

    create tab bar

---
+++ app/src/main/java/com/playlab/superpomodoro/ui/components/TabBar.kt
@@ -0,0 +1,157 @@
+package com.playlab.superpomodoro.ui.components
+
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Games
+import androidx.compose.material.icons.rounded.Chat
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.playlab.superpomodoro.R
+import com.playlab.superpomodoro.ui.theme.Blue200
+import com.playlab.superpomodoro.ui.theme.Green100
+import com.playlab.superpomodoro.ui.theme.Purple400
+import com.playlab.superpomodoro.ui.theme.SuperPomodoroTheme
+
+enum class TabPage {
+    Home, Chat, Games
+}
+
+@Composable
+fun HomeTabBar(
+    modifier: Modifier = Modifier,
+    tabPage: TabPage,
+    backgroundColor: Color = MaterialTheme.colors.background,
+    onTabSelected: (tabPage: TabPage) -> Unit
+) {
+    TabRow(
+        modifier = modifier,
+        selectedTabIndex = tabPage.ordinal,
+        backgroundColor = backgroundColor,
+        indicator = { tabPositions ->
+            HomeTabIndicator(tabPositions, tabPage)
+        }
+    ) {
+        HomeTab(
+            icon = Icons.Rounded.Home,
+            title = stringResource(R.string.home_tab),
+            onClick = { onTabSelected(TabPage.Home) }
+        )
+        HomeTab(
+            icon = Icons.Rounded.Chat,
+            title = stringResource(R.string.chat_tab),
+            onClick = { onTabSelected(TabPage.Chat) }
+        )
+        HomeTab(
+            icon = Icons.Default.Games,
+            title = stringResource(R.string.games_tab),
+            onClick = { onTabSelected(TabPage.Games) }
+        )
+    }
+}
+
+@Composable
+private fun HomeTabIndicator(
+    tabPositions: List<TabPosition>,
+    tabPage: TabPage
+) {
+    val transition = updateTransition(
+        tabPage,
+        label = "Tab indicator"
+    )
+
+    val indicatorLeft by transition.animateDp(
+        transitionSpec = {
+            spring(stiffness = Spring.StiffnessMedium)
+        },
+        label = "Indicator left"
+    ){ page ->
+        tabPositions[page.ordinal].left
+    }
+    val indicatorRight by transition.animateDp(
+        transitionSpec = {
+            spring(stiffness = Spring.StiffnessMedium)
+        },
+        label = "Indicator right"
+    ) { page ->
+        tabPositions[page.ordinal].right
+    }
+    val color by transition.animateColor(
+        label = "Border color"
+    ) { page ->
+        when(page){
+            TabPage.Home -> Blue200
+            TabPage.Chat -> Green100
+            else -> Purple400
+        }
+    }
+
+    Box(
+        Modifier
+            .fillMaxSize()
+            .wrapContentSize(align = Alignment.BottomStart)
+            .offset(x = indicatorLeft)
+            .width(indicatorRight - indicatorLeft)
+            .padding(4.dp)
+            .fillMaxSize()
+            .border(
+                BorderStroke(2.dp, color),
+                RoundedCornerShape(4.dp)
+            )
+    )
+}
+
+@Composable
+private fun HomeTab(
+    icon: ImageVector,
+    title: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .clickable(onClick = onClick)
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(text = title)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeTabPreview() {
+    SuperPomodoroTheme(false) {
+        Surface {
+
+            var selectedTab by remember{ mutableStateOf(TabPage.Home) }
+
+            HomeTabBar(
+                tabPage = selectedTab,
+                onTabSelected = { selectedTab = it }
+            )
+        }
+    }
+}
