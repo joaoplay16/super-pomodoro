4cc313dc8956ede10a9cfb8e9b460284be22c2e2
João Pedro <joaoplay16@gmail.com>
2023-04-08 18:11:29 -0300

    set theme colors

--- app/src/main/java/com/playlab/superpomodoro/ui/theme/Theme.kt
+++ app/src/main/java/com/playlab/superpomodoro/ui/theme/Theme.kt
@@ -5,26 +5,26 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.White
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Blue200,
+    primaryVariant = Blue100,
+    secondary = Grape200
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Blue200,
+    primaryVariant = Blue100,
+    secondary = Grape200,
 
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
+    background = White,
+    surface = White,
+    onPrimary = White,
     onSecondary = Color.Black,
     onBackground = Color.Black,
     onSurface = Color.Black,
-    */
 )
 
 @Composable
