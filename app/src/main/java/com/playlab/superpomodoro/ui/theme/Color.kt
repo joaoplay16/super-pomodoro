91e03087519ae798515a873b43ea8daa2aecd5b7
João Pedro <joaoplay16@gmail.com>
2023-04-08 18:09:21 -0300

    add app colors

--- app/src/main/java/com/playlab/superpomodoro/ui/theme/Color.kt
+++ app/src/main/java/com/playlab/superpomodoro/ui/theme/Color.kt
@@ -2,7 +2,15 @@ package com.playlab.superpomodoro.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
+val Purple100 = Color(0xFFB07DD9)
+val Purple200 = Color(0xFFC28CED)
+val Purple400 = Color(0xFF6E59BE)
 val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+
+val Blue100 = Color(0XFF9DD0FF)
+val Blue200 = Color(0XFF77A5CF)
+val Grape100 = Color(0XFFB8B9DA)
+val Grape200 = Color(0XFF8285D1)
+val Gray100 = Color(0XFFDCDCDC)
+val Gray400 = Color(0XFF5D5D5D)
+val Green100 = Color(0XFF62BE59)
