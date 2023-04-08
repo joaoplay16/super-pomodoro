dea10c3e61123e680913933105db46c81c9bc7a6
João Pedro <joaoplay16@gmail.com>
2023-04-08 18:59:16 -0300

    set up app typography

--- app/src/main/java/com/playlab/superpomodoro/ui/theme/Type.kt
+++ app/src/main/java/com/playlab/superpomodoro/ui/theme/Type.kt
@@ -2,17 +2,39 @@ package com.playlab.superpomodoro.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.playlab.superpomodoro.R
 
-// Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
+        fontSize = 24.sp
+    ),
+    body2 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = FontFamily(Font(R.font.josefin_sans_regular)),
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp
+    ),
+    subtitle2 = TextStyle(
+        fontFamily = FontFamily(Font(R.font.montserrat_regular)),
+        fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
+    ),
+    h3 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp
+    ),
+
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
