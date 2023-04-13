1a85cc8a0b7c728df301e4deb37c0cd852f2e574
João Pedro <joaoplay16@gmail.com>
2023-04-13 20:53:17 -0300

    create sound effects utility class

---
+++ app/src/main/java/com/playlab/superpomodoro/util/SoundEffects.kt
@@ -0,0 +1,39 @@
+package com.playlab.superpomodoro.util
+
+import android.content.Context
+import android.media.SoundPool
+
+class SoundEffects(private val context: Context) {
+
+    private var soundPool: SoundPool? = null
+    private var soundId: Int? = null
+    private var streamId: Int? = null
+
+    fun playSound(resId: Int) {
+
+        if(soundPool == null){
+            soundPool = SoundPool.Builder().setMaxStreams(5).build()
+        }
+
+        soundPool?.let { sp ->
+            soundId = sp.load(context, resId, 1)
+
+            sp.setOnLoadCompleteListener { _, _, _ ->
+               streamId = soundPool!!.play(soundId!!, 1f, 1f, 0, 0, 1f)
+            }
+        }
+    }
+
+    fun stopSound() {
+        streamId?.let {
+            soundPool?.stop(it)
+            streamId = null
+        }
+    }
+
+    fun releaseSoundPool() {
+        soundPool?.release()
+        soundPool = null
+        soundId = null
+    }
+}
