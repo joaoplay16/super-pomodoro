362029da74c632b7471c35c3c600f9214b48f90c
João Pedro <joaoplay16@gmail.com>
2023-04-14 16:44:54 -0300

    create database module

---
+++ app/src/main/java/com/playlab/superpomodoro/di/DatabaseModule.kt
@@ -0,0 +1,28 @@
+package com.playlab.superpomodoro.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStoreFile
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+    @Provides
+    @Singleton
+    fun provideDataStore(
+        @ApplicationContext appContext: Context
+    ): DataStore<Preferences> =
+        PreferenceDataStoreFactory.create(
+            produceFile = {
+                appContext.preferencesDataStoreFile("settings")
+        }
+    )
+}
