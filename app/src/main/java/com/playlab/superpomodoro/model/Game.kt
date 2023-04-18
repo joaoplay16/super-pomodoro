7614f5b0f81335d06b113eba8e56b5ac70a94550
João Pedro <joaoplay16@gmail.com>
2023-04-18 17:35:31 -0300

    implement parcelable

--- app/src/main/java/com/playlab/superpomodoro/model/Game.kt
+++ app/src/main/java/com/playlab/superpomodoro/model/Game.kt
@@ -1,8 +1,12 @@
 package com.playlab.superpomodoro.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Game(
     val id: Int,
     val name: String,
     val url: String,
     val thumbnail: Int
-)
+) : Parcelable
