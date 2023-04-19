b800456b1cd15e5064dea33d8139c4205f854f4f
João Pedro <joaoplay16@gmail.com>
2023-04-19 20:27:47 -0300

    set app test runner

---
+++ app/src/debug/java/com/playlab/superpomodoro/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package com.playlab.superpomodoro
+
+import androidx.activity.ComponentActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : ComponentActivity()
