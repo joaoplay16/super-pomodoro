39d0d517564a90c6362f1d119fa93add846f7844
João Pedro <joaoplay16@gmail.com>
2023-04-25 12:17:15 -0300

    Add error feedback to the text input

--- app/src/main/java/com/playlab/superpomodoro/ui/components/FormInput.kt
+++ app/src/main/java/com/playlab/superpomodoro/ui/components/FormInput.kt
@@ -1,5 +1,6 @@
 package com.playlab.superpomodoro.ui.components
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
@@ -23,7 +24,9 @@ import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.playlab.superpomodoro.ui.theme.Gray100
+import com.playlab.superpomodoro.ui.theme.Red600
 import com.playlab.superpomodoro.ui.theme.SuperPomodoroTheme
 
 @Composable
@@ -38,53 +41,66 @@ fun FormInput(
     ),
     keyboardActions: KeyboardActions = KeyboardActions(),
     onTextChange: (String) -> Unit,
+    isError: Boolean = false,
+    errorMessage: String? = null
 ) {
-    OutlinedTextField(
-        modifier = modifier
-            .fillMaxWidth()
-            .semantics {
-                contentDescription = description
-            },
-        value = text,
-        shape = RoundedCornerShape(10.dp),
-        onValueChange = onTextChange,
-        placeholder = {
-            Text(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .alpha(alpha = ContentAlpha.medium),
-                text = placeholder,
-                style = MaterialTheme.typography.body2,
-                color = MaterialTheme.colors.onSurface
-            )
-        },
-        textStyle = MaterialTheme.typography.body2.copy(
-            color = MaterialTheme.colors.onSurface
-        ),
-        singleLine = true,
-        leadingIcon = {
-            IconButton(
-                modifier = Modifier
-                    .alpha(alpha = ContentAlpha.medium),
-                enabled = false,
-                onClick = {}
-            ) {
-                Icon(
-                    imageVector = leadingIcon,
-                    contentDescription = null,
-                    tint = MaterialTheme.colors.onSurface
+    Column {
+
+        OutlinedTextField(
+            modifier = modifier
+                .fillMaxWidth()
+                .semantics {
+                    contentDescription = description
+                },
+            value = text,
+            shape = RoundedCornerShape(10.dp),
+            onValueChange = onTextChange,
+            placeholder = {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .alpha(alpha = ContentAlpha.medium),
+                    text = placeholder,
+                    style = MaterialTheme.typography.body2,
+                    color = MaterialTheme.colors.onSurface
                 )
-            }
-        },
-        keyboardOptions = keyboardOptions,
-        keyboardActions = keyboardActions,
-        colors = TextFieldDefaults.textFieldColors(
-            focusedIndicatorColor = MaterialTheme.colors.primaryVariant,
-            unfocusedIndicatorColor = MaterialTheme.colors.primary,
-            backgroundColor = Gray100,
-            cursorColor = MaterialTheme.colors.onSurface
+            },
+            textStyle = MaterialTheme.typography.body2.copy(
+                color = MaterialTheme.colors.onSurface
+            ),
+            singleLine = true,
+            leadingIcon = {
+                IconButton(
+                    modifier = Modifier
+                        .alpha(alpha = ContentAlpha.medium),
+                    enabled = false,
+                    onClick = {}
+                ) {
+                    Icon(
+                        imageVector = leadingIcon,
+                        contentDescription = null,
+                        tint = MaterialTheme.colors.onSurface
+                    )
+                }
+            },
+            keyboardOptions = keyboardOptions,
+            keyboardActions = keyboardActions,
+            colors = TextFieldDefaults.textFieldColors(
+                focusedIndicatorColor = MaterialTheme.colors.primaryVariant,
+                unfocusedIndicatorColor = MaterialTheme.colors.primary,
+                backgroundColor = Gray100,
+                cursorColor = MaterialTheme.colors.onSurface
+            ),
+            isError = isError,
         )
-    )
+        if(isError && errorMessage != null){
+            Text(
+                text = errorMessage,
+                fontSize = 14.sp,
+                color = Red600
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
@@ -97,6 +113,23 @@ fun PreviewFormInput(){
                 placeholder = "E-mail",
                 leadingIcon = Icons.Default.Email,
                 onTextChange = {},
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewFormInputError(){
+    SuperPomodoroTheme {
+        Surface {
+            FormInput(
+                text = "",
+                placeholder = "E-mail",
+                leadingIcon = Icons.Default.Email,
+                onTextChange = {},
+                isError = true,
+                errorMessage = "Unknown error"
             )
         }
     }
