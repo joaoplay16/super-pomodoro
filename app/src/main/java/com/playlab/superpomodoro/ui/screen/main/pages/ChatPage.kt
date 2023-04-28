70e97c28c3fb79557bfdf47a11428e467a80b743
João Pedro <joaoplay16@gmail.com>
2023-04-27 23:22:06 -0300

    Add content to chat page

--- app/src/main/java/com/playlab/superpomodoro/ui/screen/main/pages/ChatPage.kt
+++ app/src/main/java/com/playlab/superpomodoro/ui/screen/main/pages/ChatPage.kt
@@ -2,24 +2,50 @@ package com.playlab.superpomodoro.ui.screen.main.pages
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.playlab.superpomodoro.model.Group
+import com.playlab.superpomodoro.ui.screen.ChatViewModel
 import com.playlab.superpomodoro.ui.screen.DevicesPreviews
+import com.playlab.superpomodoro.ui.screen.groups.GroupsScreen
+import com.playlab.superpomodoro.ui.screen.login.LoginScreen
 import com.playlab.superpomodoro.ui.theme.SuperPomodoroTheme
-
 
 @Composable
 fun ChatPage(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    chatViewModel: ChatViewModel? = hiltViewModel(),
+    onSignUpClick: () -> Unit,
+    onGroupSelected: (Group) -> Unit
 ) {
+
+    LaunchedEffect(key1 = Unit, block = {
+        chatViewModel?.getCurrentUser()
+    })
+
     Column(
-        modifier = modifier,
+        modifier = modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top
     ) {
-       // TODO show content
+
+        if (chatViewModel?.isLoggedIn?.value == true) {
+            GroupsScreen(
+                chatViewModel = chatViewModel,
+                onGroupSelected = onGroupSelected
+            )
+        } else {
+            LoginScreen(
+                modifier = Modifier.fillMaxSize(0.8f),
+                chatViewModel = chatViewModel,
+                onSignUpLabelClick = onSignUpClick
+            )
+        }
     }
 }
 
@@ -28,7 +54,7 @@ fun ChatPage(
 fun ChatPagePreview() {
     SuperPomodoroTheme(false) {
         Surface {
-            ChatPage()
+            ChatPage(onSignUpClick = {}, onGroupSelected = {})
         }
     }
 }
