5c9851a8d2d9b15ef33ac0aee90a18fcadfb7104
João Pedro <joaoplay16@gmail.com>
2023-04-29 20:24:06 -0300

    Create extension function to format timestamp to localized date time string

--- app/src/main/java/com/playlab/superpomodoro/util/TimeUtil.kt
+++ app/src/main/java/com/playlab/superpomodoro/util/TimeUtil.kt
@@ -1,5 +1,11 @@
 package com.playlab.superpomodoro.util
 
+import android.util.Log
+import java.time.Instant
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 import java.util.*
 import kotlin.math.floor
 
@@ -17,5 +23,13 @@ object TimeUtil {
         val second = floor((millis.toDouble() / 1000) % 60).toLong()
 
         return getFormattedTimeString( minute.toInt(), second.toInt() )
+    }
+
+    fun Long.toFormattedTimeString(): String {
+        val date = ZonedDateTime.ofInstant(Instant.ofEpochMilli(this), ZoneId.systemDefault())
+        return runCatching { DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT).format(date) }
+            .onFailure { Log.e("TIME_UTIL", "${it.message}") }
+            .getOrNull()
+            .orEmpty()
     }
 }
