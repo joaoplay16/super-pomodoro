1374e9697e447c9f844ad6d253ab1d9d311c3cd1
João Pedro <joaoplay16@gmail.com>
2023-04-30 19:59:37 -0300

    Add admin label indicator to GroupMemberItem

--- app/src/main/java/com/playlab/superpomodoro/ui/components/GroupMemberItem.kt
+++ app/src/main/java/com/playlab/superpomodoro/ui/components/GroupMemberItem.kt
@@ -4,10 +4,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -17,6 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,7 +31,8 @@ import com.playlab.superpomodoro.ui.theme.SuperPomodoroTheme
 fun GroupMemberItem(
     modifier: Modifier = Modifier,
     profilePictureUrl: String?,
-    name: String
+    name: String,
+    isAdmin: Boolean = false
 ) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
@@ -58,19 +60,28 @@ fun GroupMemberItem(
 
         Spacer(Modifier.padding(8.dp))
         Text(
-            modifier = modifier.fillMaxWidth(),
+            modifier = modifier.weight(1f),
             text = name,
             maxLines = 1,
             overflow = TextOverflow.Ellipsis,
             fontSize = 18.sp
         )
+        if (isAdmin) {
+            Text(
+                text = stringResource(id = R.string.group_member_item_admin_label),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                color = MaterialTheme.colors.primary,
+                fontSize = 16.sp
+            )
+        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun PreviewGroupMemberItem() {
-    SuperPomodoroTheme {
+    SuperPomodoroTheme(false) {
         Surface {
             Column {
                 GroupMemberItem(
@@ -79,11 +90,36 @@ fun PreviewGroupMemberItem() {
                     "https://www.fakepersongenerator.com" +
                             "/Face/female/female102157398572.jpg"
                 )
-                Spacer(modifier = Modifier.padding(8.dp))
-                //No image
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewGroupMemberItem_NoImage() {
+    SuperPomodoroTheme(false) {
+        Surface {
+            Column {
                 GroupMemberItem(
                     name = "McLovin",
                     profilePictureUrl = null
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewGroupMemberItem_Admin() {
+    SuperPomodoroTheme(false) {
+        Surface {
+            Column {
+                GroupMemberItem(
+                    name = "McLovin",
+                    profilePictureUrl = null,
+                    isAdmin = true
                 )
             }
         }
