b5a67b226aadbc86806cf44cfdf9d6246c0cf988
João Pedro <joaoplay16@gmail.com>
2023-05-01 11:26:03 -0300

    add group thumbnail image to CreateGroupDialog

--- app/src/main/java/com/playlab/superpomodoro/ui/components/CreateGroupDialog.kt
+++ app/src/main/java/com/playlab/superpomodoro/ui/components/CreateGroupDialog.kt
@@ -1,5 +1,6 @@
 package com.playlab.superpomodoro.ui.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -8,6 +9,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -21,11 +24,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
+import coil.compose.rememberAsyncImagePainter
 import com.playlab.superpomodoro.R
 import com.playlab.superpomodoro.ui.theme.Gray100
 import com.playlab.superpomodoro.ui.theme.SuperPomodoroTheme
@@ -35,6 +41,8 @@ fun CreateGroupDialog(
     modifier: Modifier = Modifier,
     onDismissRequest: () -> Unit,
     groupName: String,
+    groupThumbnail: String,
+    onThumbnailClick: () -> Unit,
     onGroupNameChange: (String) -> Unit,
     onButtonCreateClick: () -> Unit
 ) {
@@ -68,6 +76,27 @@ fun CreateGroupDialog(
                     )
                 }
                 Spacer(modifier = Modifier.padding(8.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    val p = rememberAsyncImagePainter(
+                        model = groupThumbnail,
+                        placeholder = painterResource(id = R.drawable.crowd),
+                        error = painterResource(id = R.drawable.crowd)
+                    )
+
+                    Image(
+                        modifier = Modifier
+                            .clickable { onThumbnailClick() }
+                            .size(100.dp)
+                            .clip(CircleShape),
+                        painter = p,
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop
+                    )
+                }
+                Spacer(modifier = Modifier.padding(8.dp))
                 FormInput(
                     text = groupName,
                     isError = isGroupNameValid.not(),
@@ -85,7 +114,7 @@ fun CreateGroupDialog(
                         modifier = Modifier
                             .clickable {
                                 validateGroupName()
-                                if(isGroupNameValid) onButtonCreateClick()
+                                if (isGroupNameValid) onButtonCreateClick()
                             }
                             .background(Gray100, RoundedCornerShape(16.dp))
                             .padding(horizontal = 16.dp, vertical = 8.dp)
@@ -107,7 +136,9 @@ fun PreviewCreateGroupDialog() {
         Surface {
             CreateGroupDialog(
                 groupName = "",
+                groupThumbnail = "",
                 onGroupNameChange = {},
+                onThumbnailClick = {},
                 onDismissRequest = {},
                 onButtonCreateClick = {}
             )
