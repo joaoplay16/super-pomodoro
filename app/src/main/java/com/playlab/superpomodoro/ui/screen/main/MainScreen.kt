bfdcbb949c544221e88b7eba452d288a1494377d
João Pedro <joaoplay16@gmail.com>
2023-05-03 16:04:39 -0300

    disable tabs when pomodoro is running enable when the status is break time or pomodoro is stopped

--- app/src/main/java/com/playlab/superpomodoro/ui/screen/main/MainScreen.kt
+++ app/src/main/java/com/playlab/superpomodoro/ui/screen/main/MainScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
 import com.playlab.superpomodoro.model.Game
 import com.playlab.superpomodoro.model.Group
@@ -12,6 +13,7 @@ import com.playlab.superpomodoro.ui.components.HomeTabBar
 import com.playlab.superpomodoro.ui.components.TabPage
 import com.playlab.superpomodoro.ui.screen.ChatViewModel
 import com.playlab.superpomodoro.ui.screen.PomodoroViewModel
+import com.playlab.superpomodoro.ui.screen.TimerStatus
 import com.playlab.superpomodoro.ui.screen.main.pages.ChatPage
 import com.playlab.superpomodoro.ui.screen.main.pages.GamesPage
 import com.playlab.superpomodoro.ui.screen.main.pages.HomePage
@@ -29,10 +31,34 @@ fun MainScreen(
 ) {
     var selectedTab by rememberSaveable{ mutableStateOf(TabPage.Home) }
 
+    val isPomodoroRunning = pomodoroViewModel?.isRunning
+
+    val disabledTabs = remember { mutableStateListOf<TabPage>() }
+
+    val pomodoroTimerStatus = pomodoroViewModel?.timerStatus
+
+    val lifeCycleOwner = LocalLifecycleOwner.current
+
+    LaunchedEffect(key1 = isPomodoroRunning, block = {
+        if(isPomodoroRunning == true){
+            pomodoroTimerStatus?.observe(lifeCycleOwner){ timerStatus ->
+                // enable tabs when pomodoro is in break time
+                if(timerStatus in arrayOf(TimerStatus.LONG_BREAK, TimerStatus.SHORT_BREAK))
+                    disabledTabs.clear()
+                // disable chat and games tabs
+                else disabledTabs.addAll(listOf(TabPage.Chat, TabPage.Games))
+            }
+        }else{
+            // timer is stopped, enable all tabs
+            disabledTabs.clear()
+        }
+    })
+
     Scaffold(topBar = {
         HomeTabBar(
             tabPage = selectedTab,
-            onTabSelected = { selectedTab = it }
+            onTabSelected = { selectedTab = it },
+            disabledTabs = disabledTabs
         )
     }){
         when(selectedTab){
