9e5e969b862cf5fbef77e07060554e9b43c4d02b
João Pedro <joaoplay16@gmail.com>
2023-05-02 21:02:59 -0300

    Add ads medium rectangle Banner in GamesPage

--- app/src/main/java/com/playlab/superpomodoro/ui/screen/main/pages/GamesPage.kt
+++ app/src/main/java/com/playlab/superpomodoro/ui/screen/main/pages/GamesPage.kt
@@ -1,24 +1,28 @@
 package com.playlab.superpomodoro.ui.screen.main.pages
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.google.android.gms.ads.AdSize
 import com.playlab.superpomodoro.model.Game
+import com.playlab.superpomodoro.ui.components.AdvertView
 import com.playlab.superpomodoro.ui.components.GameItem
 import com.playlab.superpomodoro.ui.screen.DevicesPreviews
 import com.playlab.superpomodoro.ui.screen.gameview.GameList
 import com.playlab.superpomodoro.ui.theme.SuperPomodoroTheme
 
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun GamesPage(
     modifier: Modifier = Modifier,
@@ -31,12 +35,16 @@ fun GamesPage(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top
     ) {
-        items(items = GameList(),  key = { it.id }){
+        itemsIndexed(items = GameList()){ index, game ->
             GameItem(
-                modifier = Modifier.clickable { onGameSelected(it) },
-                name = it.name,
-                thumbResId = it.thumbnail,
+                modifier = Modifier.clickable { onGameSelected(game) },
+                name = game.name,
+                thumbResId = game.thumbnail,
             )
+            Spacer(modifier = Modifier.padding(8.dp))
+            if (index == 0){
+                AdvertView(adSize = AdSize.MEDIUM_RECTANGLE)
+            }
             Spacer(modifier = Modifier.padding(8.dp))
         }
     }
